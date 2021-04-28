a7faff4d3b732da28821a67328509ceb744c1743
josewweee <jose>
2021-04-27 22:17:47 -0500

    adding sendSms post in FaaS

--- functions/api/sms.js
+++ functions/api/sms.js
@@ -1,39 +1,65 @@
-const { db } = require('../util/admin');
+const { db } = require('../util/admin')
+const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: 'f5ea7a63',
+  apiSecret: '4MK9DFulv9x1xf03',
+})
+
+exports.sendSms = (request, response) => {
+  const from = 'Spirt APIs'
+  const to = request.body.number
+  const text = request.body.text
+  vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (responseData.messages[0]['status'] === '0') {
+        console.log('Message sent successfully.')
+        response.json({ response: 'success' })
+      } else {
+        console.log(
+          `Message failed with error: ${responseData.messages[0]['error-text']}`
+        )
+      }
+    }
+  })
+}
 
 exports.getAllContacts = (request, response) => {
   db.collection('contacts')
     .get()
     .then((data) => {
-      let contacts = [];
+      let contacts = []
       data.forEach((doc) => {
         contacts.push({
           id: doc.id,
           name: doc.data().name,
           number: doc.data().number,
-        });
-      });
-      return response.json(contacts);
+        })
+      })
+      return response.json(contacts)
     })
     .catch((err) => {
-      console.error(err);
-      return response.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return response.status(500).json({ error: err.code })
+    })
+}
 
 exports.postOneContact = (request, response) => {
   const newContactItem = {
     name: request.body.name,
     number: request.body.number,
-  };
+  }
   db.collection('contacts')
     .add(newContactItem)
     .then((doc) => {
-      const responseContactItem = newContactItem;
-      responseContactItem.id = doc.id;
-      return response.json(responseContactItem);
+      const responseContactItem = newContactItem
+      responseContactItem.id = doc.id
+      return response.json(responseContactItem)
     })
     .catch((err) => {
-      response.status(500).json({ error: 'Something went wrong' });
-      console.error(err);
-    });
-};
+      response.status(500).json({ error: 'Something went wrong' })
+      console.error(err)
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,13 @@
-const functions = require('firebase-functions');
-const app = require('express')();
+const functions = require('firebase-functions')
+const app = require('express')()
 
-const { getAllContacts, postOneContact } = require('./api/sms');
-const { getAllEmails, postOneEmail } = require('./api/email');
+const { getAllContacts, postOneContact, sendSms } = require('./api/sms')
+const { getAllEmails, postOneEmail } = require('./api/email')
 
-app.get('/contacts', getAllContacts);
-app.post('/contact', postOneContact);
-app.get('/emails', getAllEmails);
-app.post('/email', postOneEmail);
+app.get('/contacts', getAllContacts)
+app.post('/contact', postOneContact)
+app.post('/sendsms', sendSms)
+app.get('/emails', getAllEmails)
+app.post('/email', postOneEmail)
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app)
