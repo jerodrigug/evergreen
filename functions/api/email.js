d6f0df5603236bdd4d3c0dafdbb247eeabf4972c
josewweee <jose>
2021-04-28 21:41:24 -0500

    adding email sending

--- functions/api/email.js
+++ functions/api/email.js
@@ -1,39 +1,61 @@
-const { db } = require('../util/admin');
+const sgMail = require('@sendgrid/mail')
+const { db } = require('../util/admin')
+sgMail.setApiKey(
+  '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'
+)
 
 exports.getAllEmails = (request, response) => {
   db.collection('emails')
     .get()
     .then((data) => {
-      let emails = [];
+      let emails = []
       data.forEach((doc) => {
         emails.push({
           id: doc.id,
           name: doc.data().name,
           email: doc.data().email,
-        });
-      });
-      return response.json(emails);
+        })
+      })
+      return response.json(emails)
     })
     .catch((err) => {
-      console.error(err);
-      return response.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return response.status(500).json({ error: err.code })
+    })
+}
 
 exports.postOneEmail = (request, response) => {
   const newEmailItem = {
     name: request.body.name,
     email: request.body.email,
-  };
+  }
   db.collection('emails')
     .add(newEmailItem)
     .then((doc) => {
-      const responseEmailItem = newEmailItem;
-      responseEmailItem.id = doc.id;
-      return response.json(responseEmailItem);
+      const responseEmailItem = newEmailItem
+      responseEmailItem.id = doc.id
+      return response.json(responseEmailItem)
     })
     .catch((err) => {
-      response.status(500).json({ error: 'Something went wrong' });
-      console.error(err);
-    });
-};
+      response.status(500).json({ error: 'Something went wrong' })
+      console.error(err)
+    })
+}
+
+exports.sendEmail = (request, response) => {
+  const msg = {
+    to: request.body.email,
+    from: 'josemb125@gmail.com',
+    subject: 'Spirit te envio un mensaje bebe',
+    text: request.body.text,
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+      return response.json({ response: 'Email sent pai' })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,16 @@
 const functions = require('firebase-functions')
 const app = require('express')()
+const cors = require('cors')
+app.use(cors())
 
 const { getAllContacts, postOneContact, sendSms } = require('./api/sms')
-const { getAllEmails, postOneEmail } = require('./api/email')
+const { getAllEmails, postOneEmail, sendEmail } = require('./api/email')
 
 app.get('/contacts', getAllContacts)
 app.post('/contact', postOneContact)
 app.post('/sendsms', sendSms)
 app.get('/emails', getAllEmails)
 app.post('/email', postOneEmail)
+app.post('/sendemail', sendEmail)
 
 exports.api = functions.https.onRequest(app)
