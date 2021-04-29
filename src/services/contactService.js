d8ff83fd7658732d480f76c01b5c8e8834f80ea8
josewweee <jose>
2021-04-28 22:08:49 -0500

    adding sendSms

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -11,3 +11,31 @@ const postOneContact = async (name, number) => {
     console.error(error)
   }
 }
+
+const postOneContact = async (name, number) => {
+  const data = JSON.stringify({ name, number })
+  try {
+    const response = await axios.post(
+      `https://us-central1-topicostelematica.cloudfunctions.net/api/contact`,
+      data
+    )
+    console.log('contacto posteado')
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const sendSms = async (number, text) => {
+  const data = JSON.stringify({ number, text })
+  try {
+    const response = await axios.post(
+      `https://us-central1-topicostelematica.cloudfunctions.net/api/sendsms`,
+      data
+    )
+    console.log('sms enviado')
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
